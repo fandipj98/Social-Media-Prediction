5651fd6d137671bfa1e3c1cc431a775d4b0392fb
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2019-10-30 15:01:25 +0700

    final tugas KNN

--- KNN_tugas.py
+++ KNN_tugas.py
@@ -7,7 +7,7 @@ def loadDataset(filename, filenameTest, split, trainingSet=[] , testSet=[]):
     with open(filename, 'r') as csvfile:
         lines = csv.reader(csvfile)
         dataset = list(lines)
-        for x in range(1500):
+        for x in range(153):
             trainingSet.append(dataset[x])
     with open(filenameTest, 'r') as csvfileTest:
 	    linesTest = csv.reader(csvfileTest)
@@ -36,7 +36,7 @@ def getNeighbors(trainingSet, testInstance, k):
         dist += float(hammingDistance(testInstance, trainingSet[x], 1, 3))
         dist += float(hammingDistance(testInstance, trainingSet[x], 2, 2))
         dist += euclideanDistance(testInstance, trainingSet[x], 3, 0)
-        # dist += euclideanDistance(testInstance, trainingSet[x], 4, 4)
+        dist += euclideanDistance(testInstance, trainingSet[x], 4, 4)/10000000
         # print (dist)
         distances.append((trainingSet[x], dist))
     # print (distances[0][0])
@@ -65,7 +65,7 @@ def getAccuracy(testSet, predictions):
 def getMSE(testSet, predictions):
 	nilai = 0.0
 	for x in range(len(testSet)):
-		nilai += pow((float(testSet[x][-1]) - float(predictions[x])), 2)
+		nilai += pow((float(testSet[x]) - float(predictions[x])), 2)
 	return nilai/float(len(testSet))
 	
 def main():
@@ -78,16 +78,30 @@ def main():
     print ('Test set: ' + repr(len(testSet)))
 	# generate predictions
     predictions=[]
-    k = int(math.sqrt(len(testSet)))
+    rate = 0.0
+    # k = int(math.sqrt(len(testSet)))
+    k = 9
     print ('Nilai K: ' + repr(k))
+    
+    newFile = open("17-041_17-053_17-056_KNN.csv", "a")
+    
     for x in range(len(testSet)):
         # print(testSet[x])
         neighbors = getNeighbors(trainingSet, testSet[x], k)
         result = getResponse(neighbors, k)
         predictions.append(result)
+        newFile.write(str(result) + '\n')
         print('> predicted=' + repr(result))
-	# mse = getMSE(testSet, predictions)
-	# print('MSE: ' + repr(mse))
+        rate += result
+    rate /= len(testSet)
+    newFile.close()
+    # print (rate)
+    # cobaSet = [6.51,8.63,11.17,15.15,8.67,11.17,11.17,16.3,15.15,15.15,8.67,15.15,15.15,15.15,15.15,15.15,15.15,15.15,6.51,6.51,15.15,6.51,6.31,8.63,16.3,16.3,9.025,15.15,15.15,8.63,15.15,15.15,8.63,8.63,11.17,15.15,11.17,15.15,8.63,6.31,11.17,6.51,6.51,8.63,15.15,8.63,16.3,8.67,8.67,8.63,15.15]
+    # cobaSet = [6.94,11.17,11.17,13.7,11.17,11.17,11.17,16.3,13.7,13.7,11.17,13.7,13.7,13.7,13.7,13.7,13.7,13.7,6.51,6.51,13.7,6.51,11.17,11.17,16.3,16.3,5.32,13.7,13.7,11.17,13.7,13.7,11.17,11.17,11.17,13.7,11.17,13.7,11.17,12.74,11.17,6.51,6.51,8.63,13.7,11.17,16.3,11.17,11.17,8.63,13.7]
+    # cobaSet = [6.94,11.17,11.17,10.7,11.17,11.17,11.17,16.3,8.485,8.485,11.17,10.7,10.7,8.485,10.7,8.485,10.7,10.7,6.51,6.51,10.7,6.51,11.17,11.17,5.88,5.88,8.18,10.7,10.63,11.17,10.63,8.485,11.17,11.17,11.17,10.7,11.17,10.7,11.17,7.78,11.17,6.51,6.51,7.78,10.7,11.17,10.3,11.17,11.17,7.78,8.485]
+    # cobaSet = [6.94,11.17,11.17,13.7,11.17,11.17,11.17,16.3,13.7,13.7,11.17,13.7,13.7,13.7,13.7,13.7,13.7,13.7,6.51,6.51,13.7,6.51,11.17,11.17,16.3,16.3,5.32,13.7,13.7,11.17,13.7,13.7,11.17,11.17,11.17,13.7,11.17,13.7,11.17,12.74,11.17,6.51,6.51,8.63,13.7,11.17,16.3,11.17,11.17,8.63,13.7]
+    # mse = getMSE(cobaSet, predictions)
+    # print('MSE: ' + repr(mse))
 	# print('Accuracy: ' + repr(accuracy))
 	
 main()
